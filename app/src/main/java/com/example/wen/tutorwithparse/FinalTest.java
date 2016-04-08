266200c68997f060dfd49ac8146e9f6b2905af9a
benmath <bensonmthw@gmail.com>
2016-04-08 16:47:31 -0500

    commit test

--- app/src/main/java/com/example/wen/tutorwithparse/FinalTest.java
+++ app/src/main/java/com/example/wen/tutorwithparse/FinalTest.java
@@ -3,6 +3,8 @@ package com.example.wen.tutorwithparse;
 /**
  * Created by wenhe on 4/8/16.
  */
-public class FinalTest {
-
+public class FinalTest{
+    public FinalTest() {
+        System.out.println("THIS BETTER WORK!!!!!");
+    }
 }
